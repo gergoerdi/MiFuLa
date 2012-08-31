5836491390f85b436e850186bfb686f097592b73
Dr. ERDI Gergo <gergo@erdi.hu>
2012-08-31 21:56:03 +0800

    Print results of parsing and scope checking

--- src/test.hs
+++ src/test.hs
@@ -1,10 +1,15 @@
 {-# LANGUAGE DataKinds #-}
 import Mifula.Syntax
 import Mifula.Parse (defs, parseWhole)
+import Mifula.Scope (scopeDefsT)
+import Mifula.Scope.SC (runSC)
 
 import qualified Text.ParserCombinators.Parsec.IndentParser as IP
 
 import Text.PrettyPrint.Leijen
+
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -12,6 +17,7 @@ import Data.Monoid
 import Prelude hiding (mapM)
 import Data.Default
 
+foo :: Tagged Defs Parsed
 foo = parseD prog
   where
     prog = unlines [ "id x = x"
@@ -19,13 +25,19 @@ foo = parseD prog
                    , "const x _ = x"
                    , "map f (Cons x xs) = Cons (f x) (mup f xs)"
                    , "map f Nil = Nil"
+                   , "mapConst = map const"
                    , "mup f (Cons x xs) = Cons (f x) (map f xs)"
                    , "mup f Nil = Nil"
-                   , "mapConst = map const"
                    ]
 
     parseD s = case IP.parse (parseWhole defs) "" s of
         Right x -> x
         Left err -> error (show err)
 
-main = print foo
+main = do
+    print $ pretty foo
+    putStrLn "--==================--"
+    let foo' = case runSC (Set.fromList ["Cons", "Nil"]) $ scopeDefsT foo of
+            Left err -> error $ show err
+            Right (foo', _) -> foo'
+    print $ pretty foo'
